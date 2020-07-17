2c7c0da351d3ef8d67ab894013f7fe8801dd9314
José Luis Ramírez Morales <sach13lrm@gmail.com>
2020-07-17 16:02:17 -0500

    Primer Commit

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+import MediaPlayer from './MediaPlayer.js';
+//import AutoPlay from './plugins/AutoPlay.js';
+
+const video = document.querySelector('video');
+const buttonPlay = document.querySelector('#play');
+const buttonMute = document.querySelector('#mute');
+/* const player = new MediaPlayer({ element: video, plugins: [new AutoPlay()] }); */
+const player = new MediaPlayer({ element: video, plugins: [] });
+
+buttonPlay.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
